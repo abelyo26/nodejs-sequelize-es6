d40b3d5e3cc87bcea444b40fae9078ce93d73e13
Abel <abelyo26@gmail.com>
2023-05-21 09:33:48 +0300

    husky,prettier and eslint setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import express, { json } from "express";
-import db from "./database/models";
+import express, { json } from 'express';
+import db from './database/models';
 
 const app = express();
 
 app.use(json());
 
-const PORT = process.env.PORT || 3000;
-
-app.get("/", async (req, res) => {
-  res.json({ status: true, message: "Our node.js app works" });
+app.get('/', async (req, res) => {
+  res.json({ status: true, message: 'Our node.js app works' });
 });
 
 db.sequelize
   .sync()
   .then(() =>
-    app.listen(5000, () => console.log("server listening on port 5000"))
+    app.listen(5000, () => console.log('server listening on port 5000'))
   );
